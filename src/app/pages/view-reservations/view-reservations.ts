b2d41dab30af8950e7b04c69a974d34ca1437d69
Dereck81 <derecknova23@gmail.com>
2025-11-10 11:25:05 -0500

    Se añadió ver reservaciones

---
+++ src/app/pages/view-reservations/view-reservations.ts
@@ -0,0 +1,74 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Reservation, ReservationService } from '../../services/reservation/reservation.service';
+import { Button } from '../../components/button/button';
+
+@Component({
+  selector: 'app-view-reservations',
+  standalone: true,
+  imports: [CommonModule, FormsModule, Button],
+  templateUrl: './view-reservations.html',
+  styleUrl: './view-reservations.css',
+})
+export class ViewReservations {
+  private reservationService = inject(ReservationService);
+  reservations: Reservation[] = [];
+
+  cargando: boolean = false;
+  errorMsg: string | null = null;
+
+  // Filtros
+  usuarioId?: number;
+  canchaId?: number;
+  estado: string = '';
+  dni: string = '';
+
+  ngOnInit() {
+    this.obtenerListado();
+  }
+
+  filtrar(): void {
+    this.obtenerListado();
+  }
+
+  obtenerListado(): void {
+    this.errorMsg = null;
+    this.cargando = true;
+
+    const filters = {
+      usuarioId: this.usuarioId,
+      canchaId: this.canchaId,
+      estado: this.estado || undefined,
+      dni: this.dni || undefined,
+    };
+
+    this.reservationService.getListReservationCashier(filters).subscribe({
+      next: (data) => {
+        this.reservations = data.content;
+        this.cargando = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.errorMsg = 'Ocurrió un error al obtener el listado. Intenta nuevamente.';
+        this.cargando = false;
+      },
+    });
+  }
+
+  getEstadoClass(estado: string): string {
+    const baseClasses = 'inline-block rounded-full px-2 py-1 text-xs font-medium';
+    
+    switch (estado) {
+      case 'CONFIRMADA':
+      case 'COMPLETADA':
+        return `${baseClasses} bg-green-100 text-green-800`;
+      case 'PENDIENTE':
+        return `${baseClasses} bg-yellow-100 text-yellow-800`;
+      case 'CANCELADA':
+        return `${baseClasses} bg-red-100 text-red-800`;
+      default:
+        return `${baseClasses} bg-gray-100 text-gray-800`;
+    }
+  }
+}
