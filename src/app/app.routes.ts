ccf758eae3af7393818662ff0a2dfe3ce2a610b7
Diego Llacsahuanga <llacsahuanga.buques@gmail.com>
2025-12-01 02:49:35 -0500

    fix: redirect to mis-reservaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -66,6 +66,11 @@ export const routes: Routes = [
     canActivate: [isLoggedInGuard],
     children: [
       {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'mis-reservaciones',
+      },
+      {
         path: 'mis-reservaciones',
         component: MisReservaciones,
         data: { breadcrumb: 'Mis reservaciones' }
